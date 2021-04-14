ea56dfc2908a3cfe311da5f15c3ce959d22c94f2
Francis Featherby <ffrancis@seas.upenn.edu>
2021-04-14 20:44:14 +0100

    Added commodity group buttons

--- app/client/src/components/Search.js
+++ app/client/src/components/Search.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react'
-import GroupButton from './GroupButton';
+import React from 'react';
+import '../style/Dashboard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SectorButton from './SectorButton';
 
-class Search extends Component {
+export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
 
-    // The state maintained by this React Component. This component maintains the list of commodity groups.
+    // The state maintained by this React Component. This component maintains the list of genres,
+    // and a list of movies for a specified genre.
     this.state = {
-      commodity_groups: []
+      genres: [],
     }
 
-    this.showSearchOptions = this.showSearchOptions.bind(this);
-  
   }
 
-  // React function that is called when the page loads.
+  // React function that is called when the page load.
   componentDidMount() {
     // Send an HTTP request to the server.
-    fetch("http://localhost:8081/commodity_groups",
+    fetch("http://localhost:5000/commodity_groups",
     {
       method: 'GET' // The type of HTTP request.
     }).then(res => {
@@ -26,16 +27,16 @@ class Search extends Component {
     }, err => {
       // Print the error if there is one.
       console.log(err);
-    }).then(groupList => {
-      if (!groupList) return;
-      // Map each commodity group in this.state.commodity_groups to an HTML element:
+    }).then(genreList => {
+      if (!genreList) return;
+      // Map each genre in this.state.genres to an HTML element:
       // A button which triggers the showMovies function for each genre.
-      let groupDivs = groupList.map((groupObj, i) =>
-	<GroupButton id={"button-" + groupObj.group} onClick={() => this.showSearchOptions(groupObj.group)} group={groupObj.group_name} /> );
+      let genreDivs = genreList.map((genreObj, i) =>
+	<SectorButton id={"button-" + genreObj.group_name} onClick={() => this.showSearchOptions(genreObj.group_name)} genre={genreObj.group_name} /> );
 
       // Set the state of the genres list to the value returned by the HTTP response from the server.
       this.setState({
-        groups: groupDivs
+        genres: genreDivs
       });
     }, err => {
       // Print the error if there is one.
@@ -43,28 +44,23 @@ class Search extends Component {
     });
   }
 
-  showSearchOptions(group) {
-    
-  }
 
-
-  render() {
-    
+  render() {    
     return (
-      <div className="content">
-        <div className="container-fluid">
+      <div className="Dashboard">
+
+        <br></br>
+        <div className="container movies-container">
           <div className="jumbotron">
-            <div className="input-container">
-            <div className="h5">What sector do you want to search?</div>
-            <div className="commodity_groups-container">
-              {this.state.commodity_groups}
+            <div className="h5">Which sector do you want to search?</div>
+            <div className="genres-container">
+              {this.state.genres}
             </div>
-			    	</div>
           </div>
+
+          
         </div>
       </div>
-    )
+    );
   }
 }
-
-export default Search
