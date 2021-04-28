50bcb2cc09b7a0c33ea448dea1276d8c2dc81d84
Francis Featherby <ffrancis@seas.upenn.edu>
2021-04-28 17:18:21 +0100

    Changes to Search presentation.
    Problem remains with results box.

--- app/client/src/components/SearchResultRow.js
+++ app/client/src/components/SearchResultRow.js
@@ -10,7 +10,6 @@ export default class SearchResultRow extends React.Component {
 		return (
 			<div className="searchResults">
 				<div className="year">{this.props.year}</div>
-				<div className="beginning_stocks">{this.props.beginning_stocks}</div>
 				<div className="production">{this.props.production}</div>
 				<div className="consumption">{this.props.consumption}</div>
 				<div className="ending_stocks">{this.props.ending_stocks}</div>
