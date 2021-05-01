30626b309bb09f9d240a0221c662eef19df3aa3b
Iris Tiong <57525047+iristyx@users.noreply.github.com>
2021-05-01 01:50:25 -0700

    update homepage

--- app/client/src/components/Home.js
+++ app/client/src/components/Home.js
@@ -1,8 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
 export default class Home extends Component {
     render() {
-        return (<p>Hi</p>)
+        return (
+            <div class="container-fluid">
+                
+                <div class="row">
+                    <div class="col-sm-4 my-auto">
+                        <h2><strong>It has never been easier to understand the global commodities market.</strong></h2><br/>
+                        Our project is an innovation because existing resources deal mostly with individual sectors like energy or agriculture, but not the the whole commodities space, and they are read only. Our project provides a useful resource for trade houses, banks and hedge funds.
+
+
+                    </div>
+
+                    <div class="col-sm-8">
+                        <img src="home-1.jpg" class="img-fluid"></img>
+                    </div>
+
+                </div>
+            </div>
+        )
     }
 }
